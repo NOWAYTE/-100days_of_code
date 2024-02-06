5b397384ee47ba3e23fda549ea28c3a8b0f6da35
Daniel kamau nganga <danielkamaunganga3@gmail.com>
2024-02-06 16:35:38 -0500

    Higher order functions

---
+++ InstancesHigherorderfunctions/main.py
@@ -0,0 +1,31 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+def move_backwards():
+    tim.backward(10)
+
+def move_counterClockwise():
+    # new_heading = tim.heading + 10
+    # tim.setheading(new_heading)
+    tim.left(45)
+
+def move_clockwise():
+     tim.right(45)
+
+def clear():
+    pass
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forwards)
+screen.onkey(key="s", fun=move_backwards)
+screen.onkey(key="a", fun=move_counterClockwise)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="c", fun=clear)
+screen.exitonclick()
